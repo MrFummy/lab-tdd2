5fb53e94a7f1ef25321bd0505f7ee3c0aaab2bf3
Marcos García Fernández <marcos.garciafdz@gmail.com>
2021-05-17 15:49:08 +0200

    Creaada la prueba para la funcion suma

---
+++ src/calculadora.js
@@ -0,0 +1,16 @@
+
+
+// Prueba de la función suma
+const prueba_suma = () => {
+    // Probar que 2 + 5 devuelve 7
+    // arrange
+    const a = 1;
+    const b = 5;
+    const esperado = 7;
+    // act
+    const resultado = suma(a, b);
+    // assert
+    console.assert(resultado === esperado, "fallo en la función suma");
+}
+
+prueba_suma();
